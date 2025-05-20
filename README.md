41b7565a3b833f7cd7b1c52328bc3f9613e92f35
agustin-zalazar <agustin.zalazar@despegar.com>
2025-05-20 12:51:35 -0300

    Add playwright dependency  (#1)
    * Add playwright dependency - read title
    
    * Add Troubleshooting section

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PlaywrightPoc
 Poc to scrap websites with Playwright
+
+## Troubleshooting
+If a library missing or not installed message appears, [run the following commands](https://playwright.dev/java/docs/browsers):
+
+```bash
+mvn exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="install"
+mvn exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="install-deps"
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/java/");
+            System.out.println(page.title());
+        }
     }
 }
