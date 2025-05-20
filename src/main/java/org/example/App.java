5f69c41c96cf44651d681fd0dc107b012da3360e
agustin-zalazar <agustin.zalazar@despegar.com>
2025-05-20 13:27:34 -0300

    Try to parse a non working site

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,20 +1,26 @@
 package org.example;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.LoadState;
 
 import java.nio.file.Paths;
+import java.util.Collections;
 
 public class App 
 {
     public static void main( String[] args )
     {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.webkit().launch();
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions()
+                            .setHeadless(true)
+                            .setArgs(Collections.singletonList("--no-sandbox"))
+            );
             Page page = browser.newPage();
-            page.navigate("https://playwright.dev/");
+            page.navigate("https://www.afkl.biz/trade/s/resources-detail?Id=000001277&language=pt_PT&country=BR#");
+            page.waitForLoadState(LoadState.NETWORKIDLE);  // esperar a que no haya más peticiones
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+            System.out.println("Page content:" + page.content());
         }
     }
 }
